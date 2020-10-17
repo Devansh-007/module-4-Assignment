06148f30c6dd89b6e9980002cd533f2794fbfed0
Devansh-007 <71151176+Devansh-007@users.noreply.github.com>
2020-10-17 22:33:48 +0530

    Update Dscript.js

--- Dscript.js
+++ Dscript.js
@@ -31,7 +31,8 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-    var helloSpeaker= {}; 
+( function (names)   {
+var helloSpeaker= {}; 
     var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     
     // STEP 10: (NOTHING TO DO. ALREADY DONE FOR YOU)
@@ -61,5 +62,4 @@ WARNING!!! WARNING!!!
         helloSpeaker.speak(names[i]);
       }
     }
-    
-   
+}(names);   
